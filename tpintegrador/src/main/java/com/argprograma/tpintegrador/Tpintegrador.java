0d2da8334aa5b7e12e7c23f01a0f3afdbcbb5bcb
FrancoBalsamo <70456335+FrancoBalsamo@users.noreply.github.com>
2023-03-21 16:45:50 -0300

    Se actualiza la main principal
    Se añade la función para que se lea el csv

--- tpintegrador/src/main/java/com/argprograma/tpintegrador/Tpintegrador.java
+++ tpintegrador/src/main/java/com/argprograma/tpintegrador/Tpintegrador.java
@@ -4,13 +4,25 @@
 
 package com.argprograma.tpintegrador;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 /**
  *
  * @author francobalsamo
  */
 public class Tpintegrador {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws FileNotFoundException {        
+        archivos();
+    }
+    
+    public static void archivos() throws FileNotFoundException{
+        Scanner entrada = new Scanner(new FileReader("C:\\Users\\francobalsamo\\Downloads\\resultados.csv"));
+        while(entrada.hasNext()){
+            String lineas = entrada.nextLine();
+            System.out.println(lineas);
+        }
     }
 }
