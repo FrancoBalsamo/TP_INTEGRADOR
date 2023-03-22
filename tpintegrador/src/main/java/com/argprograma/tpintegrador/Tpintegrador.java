4443484856a0d408af9b719200d9eced7411c279
FrancoBalsamo <70456335+FrancoBalsamo@users.noreply.github.com>
2023-03-22 11:52:21 -0300

    Update
    Se modifica la función que lee el csv, se cambia scanner por bufferedreader para omitir la pirmer línea y se instancian los objetos

--- tpintegrador/src/main/java/com/argprograma/tpintegrador/Tpintegrador.java
+++ tpintegrador/src/main/java/com/argprograma/tpintegrador/Tpintegrador.java
@@ -4,9 +4,13 @@
 
 package com.argprograma.tpintegrador;
 
+import com.argprograma.tpintegrador.clases.Equipo;
+import com.argprograma.tpintegrador.clases.Partido;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Scanner;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,15 +19,31 @@ import java.util.Scanner;
 public class Tpintegrador {
 
     public static void main(String[] args) throws FileNotFoundException {        
-        archivos();
+        System.out.println(archivos().toString());
     }
     
-    public static void archivos() throws FileNotFoundException{
-        //Scanner entrada = new Scanner(new FileReader("C:\\Users\\Franco Balsamo\\Documents\\GitHub\\TP_INTEGRADOR\\resultados.csv"));
-        Scanner entrada = new Scanner(new FileReader("C:\\Users\\Franco Balsamo\\Documents\\GitHub\\TP_INTEGRADOR\\pronostico.csv"));
-        while(entrada.hasNext()){
-            String lineas = entrada.nextLine();
-            System.out.println(lineas);
+    private static ArrayList<Partido> archivos() throws FileNotFoundException{
+        //Scanner entrada = new Scanner(new FileReader("C:\\Users\\FrancoBalsamo\\Documents\\GitHub\\TP_INTEGRADOR\\resultados.csv"));
+        //Scanner entrada = new Scanner(new FileReader("C:\\Users\\Franco Balsamo\\Documents\\GitHub\\TP_INTEGRADOR\\pronostico.csv"));
+        BufferedReader br;
+        ArrayList<Partido> arrayPartido = new ArrayList<>();
+        try{
+            br = new BufferedReader(new FileReader("C:\\Users\\FrancoBalsamo\\Documents\\GitHub\\TP_INTEGRADOR\\resultados.csv"));
+            br.readLine();
+            String linea;
+            while((linea = br.readLine()) != null){
+                String[] arrayComas = linea.split(",");
+
+                Equipo equipo1 = new Equipo(arrayComas[0], "descripción");
+                Equipo equipo2 = new Equipo(arrayComas[3], "descripción");
+
+                arrayPartido.add(new Partido(equipo1, equipo2, Integer.parseInt(arrayComas[1]), Integer.parseInt(arrayComas[2])));
+            }
+        }catch(FileNotFoundException fnfe){
+            System.out.println(fnfe.getMessage());
+        }catch(IOException ioe){
+            System.out.println(ioe.getMessage());
         }
+        return arrayPartido;
     }
 }
